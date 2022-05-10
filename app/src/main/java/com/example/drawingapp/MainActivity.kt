224396a159efbd72ea1a311071dc10772337b657
ukenov <ykenovalex97@gmail.com>
2022-05-10 19:59:57 +0500

    Add galery

--- app/src/main/java/com/example/drawingapp/MainActivity.kt
+++ app/src/main/java/com/example/drawingapp/MainActivity.kt
@@ -1,20 +1,28 @@
 package com.example.drawingapp
 
+import android.Manifest
+import android.app.Activity
 import android.app.Dialog
+import android.content.Intent
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore
 import android.view.View
 import android.widget.ImageButton
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatDelegate
+import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import androidx.core.view.get
 import com.example.drawingapp.databinding.ActivityMainBinding
 import com.example.drawingapp.databinding.DialogBrushSizeBinding
+import java.lang.Exception
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
-    private lateinit var bindingDialog: DialogBrushSizeBinding
 
     private var mImageButtonCurrentPaint: ImageButton? = null
 
@@ -22,7 +30,6 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_UNSPECIFIED)
         binding = ActivityMainBinding.inflate(layoutInflater)
-        bindingDialog = DialogBrushSizeBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
         mImageButtonCurrentPaint = binding.paintColors[1] as ImageButton
@@ -34,13 +41,69 @@ class MainActivity : AppCompatActivity() {
         binding.ibBrush.setOnClickListener{
             showBrushSizeChooserDialog()
         }
+        binding.ibGallery.setOnClickListener {
+            if(isReadStorageAllowed()) {
+                val pickPhotoIntent = Intent(
+                    Intent.ACTION_PICK,
+                    MediaStore.Images.Media.EXTERNAL_CONTENT_URI
+                )
+
+                pickPhotoActivityForResult.launch(pickPhotoIntent)
+            } else {
+                Toast.makeText(this, "Per requested", Toast.LENGTH_SHORT).show()
+                requestStoragePermission()
+            }
+        }
+    }
+
+    private val pickPhotoActivityForResult = registerForActivityResult(
+        ActivityResultContracts.StartActivityForResult()) { result ->
+        if(result.resultCode == Activity.RESULT_OK) {
+            try {
+                val data: Intent? = result.data
+                if(data != null) {
+                    binding.ivBackground.visibility = View.VISIBLE
+                    binding.ivBackground.setImageURI(data.data)
+                } else {
+                    Toast.makeText(this@MainActivity, "Error while pasting image", Toast.LENGTH_SHORT).show()
+                }
+            } catch (e: Exception) {
+                e.printStackTrace()
+            }
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+
+        if(requestCode == STORAGE_PERMISSION_CODE) {
+            if(grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                Toast.makeText(this@MainActivity, "Permissions granted", Toast.LENGTH_SHORT).show()
+            } else {
+                Toast.makeText(this@MainActivity, "Permissions denied", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+    private fun isReadStorageAllowed(): Boolean {
+        val result = ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE)
+
+        return result == PackageManager.PERMISSION_GRANTED
     }
 
     private fun showBrushSizeChooserDialog() {
         val brushDialog = Dialog(this)
-        val dialogView = bindingDialog.root
-        brushDialog.setContentView(dialogView)
+        val bindingDialog = DialogBrushSizeBinding.inflate(layoutInflater)
+        val view = bindingDialog.root
+        brushDialog.setContentView(view)
         brushDialog.setTitle("Brush size: ")
+
+
+
         bindingDialog.ibSmallBrush.setOnClickListener{
             binding.drawingView.setSizeForBrush(10.toFloat())
             brushDialog.dismiss()
@@ -71,5 +134,18 @@ class MainActivity : AppCompatActivity() {
 
             mImageButtonCurrentPaint = view
         }
+    }
+
+    private fun requestStoragePermission() {
+        if(!isReadStorageAllowed()) {
+            ActivityCompat.requestPermissions(this, arrayOf(
+                Manifest.permission.READ_EXTERNAL_STORAGE,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE
+            ), STORAGE_PERMISSION_CODE)
+        }
+    }
+
+    companion object {
+        private const val STORAGE_PERMISSION_CODE = 1
     }
 }
